24efedcfd7dd38f7bffdedae110a1155bee5c7b6
Shane Graff <copellius@gmail.com>
2022-01-09 09:53:43 -0800

    Add functionality to Add Tickety button

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import TicketList from "./TicketControl";
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <TicketList />
+    </React.Fragment>
+  );
+}
+
+export default App;
+
